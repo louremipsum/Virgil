f6621121eb8661d41839d7623657b8b899026eeb
louremipsum <myself.vinayak2@gmail.com>
2023-05-14 17:31:07 +0530

    feat: React-Router init with routes

--- Virgil-Frontend/src/main.jsx
+++ Virgil-Frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "../src/App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./routes/Error";
+import Search from "./routes/Search";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+    errorElement: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
